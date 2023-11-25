c41b6803c0abcc718c1ddc447466021a45e2c218
piumra938 <151438656+piumra938@users.noreply.github.com>
2023-11-25 19:57:22 +0530

    Add files via upload

---
+++ Is it a prime number.py
@@ -0,0 +1,19 @@
+def isPrimeNumber(): #new function 
+ x = int(input("Enter a number: ")) #ask for user input by (Enter a number)
+ if x == 10001 : #Easter egg to the last problem that you sent 
+     print('we dont do that here')
+ current_number = 1  # Starting number
+ divison_count = 0  # Starting number
+ while current_number <= x:  # Change the condition to run until current_number reaches x
+    division_reminder = x % current_number # division by modulo operator
+    if division_reminder == 0 :
+     divison_count += 1
+    current_number += 1
+ if divison_count == 2 or divison_count == 1 :
+   return True
+ return False
+if(isPrimeNumber()):
+  print('IT IS PRIME NUMBER')
+else :print('NOT A PRIME NUMBER')
+
+
